ddc364769df1fe0be766f0dc106ad9bd14addf72
Camila Guillan <62452679+camiguillan@users.noreply.github.com>
2025-04-06 23:59:12 +0000

    adding animations with framer

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,13 +1,23 @@
 import { createPortal } from 'react-dom';
+import { motion } from 'framer-motion';
 
 export default function Modal({ title, children, onClose }) {
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
-      <dialog open className="modal">
+      <motion.dialog 
+      initial={{opacity: 0, y: 30}} //for stablishing initial state of the animation 
+      animate={{
+        opacity:1, y: 0
+      }}
+      exit={{
+        //when the element is removed from the dom
+        opacity: 0, y: 30
+      }}
+      open className="modal">
         <h2>{title}</h2>
         {children}
-      </dialog>
+      </motion.dialog>
     </>,
     document.getElementById('modal')
   );
