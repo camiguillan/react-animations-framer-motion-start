ce2801cf5f7b6a15c1e110de0f49a40206ade868
Camila Guillan <62452679+camiguillan@users.noreply.github.com>
2025-04-07 00:31:24 +0000

    animating form inputs

--- src/components/NewChallenge.jsx
+++ src/components/NewChallenge.jsx
@@ -1,5 +1,5 @@
 import { useContext, useRef, useState } from 'react';
-import { motion } from 'framer-motion';
+import { motion,useAnimate, stagger } from 'framer-motion';
 
 import { ChallengesContext } from '../store/challenges-context.jsx';
 import Modal from './Modal.jsx';
@@ -9,6 +9,7 @@ export default function NewChallenge({ onDone }) {
   const title = useRef();
   const description = useRef();
   const deadline = useRef();
+  const [scope, animate] = useAnimate();
 
   const [selectedImage, setSelectedImage] = useState(null);
   const { addChallenge } = useContext(ChallengesContext);
@@ -32,6 +33,12 @@ export default function NewChallenge({ onDone }) {
       !challenge.deadline.trim() ||
       !challenge.image
     ) {
+      animate('input, textarea', 
+        {x: [-10,0,10,0]}, 
+        {type: 'tween',
+          ease: 'easeInOut',
+          duration: 0.4,
+          delay: stagger(0.05)})
       return;
     }
 
@@ -41,7 +48,7 @@ export default function NewChallenge({ onDone }) {
 
   return (
     <Modal title="New Challenge" onClose={onDone}>
-      <form id="new-challenge" onSubmit={handleSubmit}>
+      <form id="new-challenge" onSubmit={handleSubmit} ref={scope} >
         <p>
           <label htmlFor="title">Title</label>
           <input ref={title} type="text" name="title" id="title" />
@@ -68,7 +75,7 @@ export default function NewChallenge({ onDone }) {
             <motion.li
               variants={{
                 hidden: {opacity: 0, scale: 0.5},
-                visible: {opacity: 1, scale:1}
+                visible: {opacity: 1, scale: [0.8 , 1]} //if i use an array -> array of key frames, multiple steps of an animation
               }} 
               // exit={{opacity: 1, scale:1}}
               transition={{type:'spring'}}
